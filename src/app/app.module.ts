db67e1f44f0b836671381c8173aeb2a23890b168
Mike Billings <3169366+joecoolio@users.noreply.github.com>
2022-10-17 10:41:39 -0400

    settings and cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Neccessary for the app
+import { HttpClientModule } from '@angular/common/http';
+
+// App components
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SettingsComponent } from './settings/settings.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
-
+// Material
 import { MatDialogModule } from '@angular/material/dialog';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-import { HttpClientModule } from '@angular/common/http';
 
 // import { FontAwesomeModule, FaIconLibrary } from '@fontawesome/angular-fontawesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Bootstrap stuff
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
@@ -34,10 +40,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ReactiveFormsModule,
     FontAwesomeModule,
     MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    NgbModule,
+    // MatFormFieldModule,
+    // MatInputModule,
+    // MatButtonModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    // NgbModule,
   ],
 
   declarations: [
---
+++ src/app/services/player.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AudioService {
+
+  constructor() { }
+
+// export interface UserSettings {
+//     userid: string, // User ID
+//     letters: number, // Number of letters
+//     hops: number, // Number of hops
+  
+    
+//   }
+  
+}  
