e204cb7382a9702652a1b32c902f4a537d527902
Mike Billings <3169366+joecoolio@users.noreply.github.com>
2022-10-28 14:15:49 -0400

    Preload audio

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -5,8 +5,25 @@ import { PlayerService } from './player.service';
 export class AudioService {
   private _audio;
 
+  // All of the audio files - each will be preloaded
+  private audioFiles = [
+    '../assets/sounds/9744_horn_typewriter.mp3',
+    '../assets/sounds/650842_andreas_wrong-answer-buzzer.mp3',
+    '../assets/sounds/345299_scrampunk_okay.mp3',
+    '../assets/sounds/393402_stevedrums_group-yay-cheer.mp3',
+    '../assets/sounds/172950__notr__saddertrombones.mp3',
+    '../assets/sounds/243749_unfa_metronome-1khz-weak-pulse.mp3',
+    '../assets/sounds/554053_gronkjaer_wronganswer.mp3',
+    '../assets/sounds/429720__fellur__tic.mp3'
+  ];
+
   constructor(private playerService: PlayerService) {
     this._audio = new Audio();
+
+    // Preload all the sound files
+    for (var i in this.audioFiles) {
+      this.preloadAudio(this.audioFiles[i]);
+    }
   }
 
   private playSound(filename: string) {
@@ -18,6 +35,20 @@ export class AudioService {
       this._audio.play();
     }
   }
+
+  preloadAudio(url) {
+    var audio = new Audio();
+    // once this file loads, it will call loadedAudio()
+    // the file will be kept by the browser as cache
+    audio.addEventListener('canplaythrough', this.loadedAudio, false);
+    audio.src = url;
+  }
+    
+  loadedAudio() {
+    // this will be called every time an audio file is loaded
+    // we keep track of the loaded files vs the requested files
+  }
+
 
   letterEntered(): void {
     this.playSound('../assets/sounds/9744_horn_typewriter.mp3');
