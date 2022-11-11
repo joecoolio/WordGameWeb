8307121704f3d6c8dedb0c61ba84b4e2123ddb52
Mike Billings <github@mikebillings.com>
2022-11-10 22:47:16 -0500

    storing mysql code

---
+++ database_scripts/6letterwords.sql
@@ -0,0 +1,181 @@
+LOAD DATA LOCAL INFILE '6letter1hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 1
+;
+LOAD DATA LOCAL INFILE '6letter2hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 2
+;
+LOAD DATA LOCAL INFILE '6letter3hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 3
+;
+LOAD DATA LOCAL INFILE '6letter4hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 4
+;
+LOAD DATA LOCAL INFILE '6letter5hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 5
+;
+LOAD DATA LOCAL INFILE '6letter6hop.csv' 
+INTO TABLE pair6
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 6
+;
+
+
+delete from pair6 where pair = 'pair';
+
+update pair6 set keep = 1 where minpathlength = 1 and keep = 0;
+
+update pair6 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair6 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 2
+;
+
+update pair6 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair6 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 3
+;
+
+update pair6 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair6 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 4
+;
+
+update pair6 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair6 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 5
+;
+
+update pair6 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair6 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 6
+;
+
+select pair, fullpath, minpathlength
+INTO OUTFILE '/var/lib/mysql/6_toload.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+from pair6
+where keep = 1
+and minpathlength > 1
+order by minpathlength, pair, fullpath
+;
+
+select distinct pair, minpathlength
+INTO OUTFILE '/var/lib/mysql/6_pairs.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+from pair6
+where keep = 1
+order by pair
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_1.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=61, pairoffset= (@rn := @rn + 1)
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_2.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=62, pairoffset= (@rn := @rn + 1)
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_3.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=63, pairoffset= (@rn := @rn + 1)
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_4.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=64, pairoffset= (@rn := @rn + 1)
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_5.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=65, pairoffset= (@rn := @rn + 1)
+;
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_6_6.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=66, pairoffset= (@rn := @rn + 1)
+;
+
