8d2f3a0a8c6e96dc2f5bc8363a90139f64a799fa
Mike Billings <github@mikebillings.com>
2022-12-03 17:36:52 -0500

    loading 5 letter / 6 hop stuff

---
+++ database_scripts/5letterwords.sql
@@ -0,0 +1,50 @@
+LOAD DATA LOCAL INFILE '5letter6hop.csv' 
+INTO TABLE pair5
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(@col1,@col2) set pair=@col1, fullpath=trim(both '"' from (trim(leading ' ' from @col2))), minpathlength = 6
+;
+
+delete from pair5 where pair = 'pair';
+
+update pair5 p_longer
+set keep = 1
+where not exists(
+  select 1
+  from pair5 p_shorter
+  where p_longer.pair = p_shorter.pair
+  and p_longer.minpathlength > p_shorter.minpathlength
+  and p_shorter.keep = 1
+)
+and p_longer.minpathlength = 6
+;
+
+select pair, fullpath, minpathlength
+INTO OUTFILE '/var/lib/mysql/5_toload_6.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+from pair5
+where keep = 1
+and minpathlength = 6
+order by minpathlength, pair, fullpath
+;
+
+select distinct pair, minpathlength
+INTO OUTFILE '/var/lib/mysql/5_pairs_uniq.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+from pair5
+where keep = 1
+order by pair
+;
+
+
+set @rn = 0;
+LOAD DATA LOCAL INFILE 'pairs_5_6.txt' 
+INTO TABLE allpairs
+FIELDS TERMINATED BY ',' 
+optionally enclosed BY '"'
+LINES TERMINATED BY '\n'
+(pair) set letters_hops=56, pairoffset= (@rn := @rn + 1)
+;
